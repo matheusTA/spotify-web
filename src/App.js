6cdb6b44aaebef5f52e91c140dbae77531e8966a
MatheusTA <matrajano12@gmail.com>
2019-09-13 16:37:11 -0300

    Criando estilo global da aplicacao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App = () => (
-  <div className="App">
-    <h1>hello</h1>
-  </div>
+  <>
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
