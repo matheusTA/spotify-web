d8cc30b0d3cd310a9a301e38f92a6e69a8a15720
MatheusTA <matrajano12@gmail.com>
2019-09-14 03:06:08 -0300

    Adicionado header na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
-import { Wrapper, Container } from './styles/components';
+import { Wrapper, Container, Content } from './styles/components';
 
 const App = () => (
   <Wrapper>
     <Container>
       <Sidebar />
+      <Content>
+        <Header />
+      </Content>
     </Container>
     <Player />
     <GlobalStyle />
