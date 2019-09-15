cd683b7c803ecf1b813ef2f1eb187f0b65f056f2
MatheusTA <matrajano12@gmail.com>
2019-09-15 19:16:52 -0300

    Configurando store

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import './config/reactotron';
 import { BrowserRouter } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
 import { Wrapper, Container, Content } from './styles/components';
+
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 
 
 import Routes from './routes';
+import store from './store';
 
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
 
-      <Container>
-        <Sidebar />
+        <Container>
+          <Sidebar />
 
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
 
-      </Container>
+        </Container>
 
-      <Player />
-    </Wrapper>
-    <GlobalStyle />
-  </BrowserRouter>
+        <Player />
+      </Wrapper>
+      <GlobalStyle />
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
