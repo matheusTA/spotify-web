91fc1958422c896d6318f180ecf93963ae2507fb
MatheusTA <matrajano12@gmail.com>
2019-09-19 02:04:42 -0300

    Criando componente de error

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Wrapper, Container, Content } from './styles/components';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
+import ErrorBox from './components/ErrorBox';
 
 
 import Routes from './routes';
@@ -23,6 +24,7 @@ const App = () => (
           <Sidebar />
 
           <Content>
+            <ErrorBox />
             <Header />
             <Routes />
           </Content>
